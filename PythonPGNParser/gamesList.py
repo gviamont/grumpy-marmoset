11e1e3efa4007ecc0ad75961b8fb3e7569fab1b0
Matt McClain <matt.mcclain79@gmail.com>
2021-03-29 12:02:31 -0600

    The previous commit broke the ability to add games to the csv file.  Made changes here to add games directly to the map so they could be called by writerow.

--- PythonPGNParser/gamesList.py
+++ PythonPGNParser/gamesList.py
@@ -1,46 +1,63 @@
 import CSVMaker
 
+# This function uses readlines() to read in one line at a time then builds
+# a map of data from each game and returns a list of all games and associated
+# pgn header info (aka tags) plus moves.
 def gamesList(fileName):
     fileName = fileName
     inputfile = open(fileName, "r")
-    if inputfile.mode == 'r':  # check to make sure that the file was opened
-        lines = inputfile.readlines() # use the readlines() function to read one line at a time
-        gameNum = 1 #game counter
-        moves = '' #initialize moves string
-        addMoves = False #initialize addMoves bool which is used as a switch to signal when we can record moves
-        gamesList = [] # creating a list of tuples consisting of ({headerMap}, moves)
+
+    # use the readlines() function to read one line at a time
+    if inputfile.mode == 'r':
+        lines = inputfile.readlines()
+
+        # Initialize a gamesList that includes a dictionary of Header items and add Moves to it
+        # addMoves is used to initialize addMoves bool which is used as a switch to signal when we can record moves
         # Initialize the headerMap with all fields so it can be converted to a flat file
+        gamesList = []
+        moves = ''
+        addMoves = False
         headerMap = CSVMaker.initializeGameMap()
+
+        # Read in a line at a time and detect new games based "["
         for line in lines:
-            if line[0:1] == "[": #new game detected when we find first open [
-                if addMoves: #after recording moves, need to flip the switch and print moves
-                    addMoves = False #Flip switch now that we are in a new game
-                    gameInfo = (headerMap, moves) # creating a tuple for each game
-                    gamesList.append(gameInfo) #adding the gameInfo from this game to a list of games
-                    moves = "" #empty moves string for next game
-                    gameNum += 1
+            if line[0:1] == "[":
+
+                # We run this if condition after we finish reading in the moves
+                # Record moves and append all data to gamesList, then empty moves string and reinitialize GameMap
+                if addMoves:
+                    addMoves = False
+                    headerMap["Moves"] = moves
+                    gamesList.append(headerMap)
+                    moves = ''
                     headerMap = CSVMaker.initializeGameMap()
-                keyValue = line[1:-2] # grab info between brackets
-                key = keyValue.split()[0] #assign 1st word to key
-                valueIndex = keyValue.find("\"") #find first " position
-                value = keyValue[valueIndex+1:-1] #grab value info between "s and store to value
+
+                # To build headerMap, isolate the info between the brackets and assign the 1st word to 'key'
+                # then find the first " position and isolate the data between the double quotes and store to 'value'
+                keyValue = line[1:-2]
+                key = keyValue.split()[0]
+                valueIndex = keyValue.find("\"")
+                value = keyValue[valueIndex+1:-1]
                 headerMap[key] = value
-            elif line[0:2] == "1.": #when we come across a 1. we know to start recording moves
-                addMoves = True #flip switch on to record moves
-            if addMoves: #if the switch is on then append lines to moves string
-                moves += str(line.rstrip()) #Append the moves together into a single string, removing trailing newlines
-        gameInfo = (headerMap, moves) #last game
-        gamesList.append(gameInfo) #last game
+
+            # When we come across the first move we flip the addMoves switch and begin recording moves
+            # Append the lines until we come across another "[" which tells us we are in a new game
+            # The rstrip() removes trailing new lines
+            elif line[0:2] == "1.":
+                addMoves = True
+            if addMoves:
+                moves += str(line.rstrip())
+        # Last game info
+        gameInfo = headerMap
+        gamesList.append(gameInfo)
 
         return gamesList
 
-    inputfile.close() # close the file when done
-
-def main():
-    thisGame = gamesList("KasparovFirst10Pages.pgn")
-    for s in thisGame:
-        print(s)
-    print(CSVMaker.fieldNames)
+    inputfile.close()
 
 if __name__ == "__main__":
-    main()
+    # pgnFileName = sys.argv[1]
+    pgnFileName = "k10.pgn"
+    thisGame = gamesList(pgnFileName)
+    for s in thisGame:
+        print(s)
